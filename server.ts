c67f6073320bd35332af876827f49a27e8fbc8e7
Que Nguyen <que.nguyen@ppaya.vn>
2019-08-04 10:01:11 +0700

    Prepare for deploying

--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import connectDB from './config/db';
 
+const path = require('path');
 const app: Application = express();
 // Connect DB
 connectDB();
@@ -16,6 +17,15 @@ app.get('/', (req: Request, res: Response) =>
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static asset in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req: Request, res: Response) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
